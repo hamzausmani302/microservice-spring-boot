56035d1793e2f4ba273ca1ec291c45c3b003337e
hamzausmani302 <hamzausmani021@gmail.com>
2023-04-07 16:48:21 +0500

    added invenotry service and order service

---
+++ InventoryService/src/test/java/com/hamza/InventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.InventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
